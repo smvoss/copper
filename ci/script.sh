001582ee5a63844e8fe128fe2bb7eaa706533f74
Jorge Aparicio <japaricious@gmail.com>
2016-12-31 14:36:06 -0500

    use `cross`

--- ci/script.sh
+++ ci/script.sh
@@ -1,8 +1,8 @@
 set -ex
 
-test_mode() {
+main() {
+    local target=
     for app in $(echo app/*); do
-        local target=
         case $app in
             app/01-qemu)
                 target=thumbv7m-none-eabi
@@ -12,20 +12,14 @@ test_mode() {
                 ;;
         esac
 
-        cd $app
-        xargo build --target $target
+        pushd $app
+        cross build --target $target
         arm-none-eabi-objdump -Cd target/$target/debug/app
-        cd ../..
+        popd
     done
 
-    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        mdbook build
-        linkchecker book
-    fi
+    mdbook build
+    linkchecker book
 }
 
-run() {
-    test_mode
-}
-
-run
+main
