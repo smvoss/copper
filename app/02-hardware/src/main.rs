26800d4019d3b024c5c99726133195f129341926
Jorge Aparicio <japaricious@gmail.com>
2016-12-31 18:36:38 -0500

    don't use symlinks

--- app/02-hardware/src/main.rs
+++ app/02-hardware/src/main.rs
@@ -1,1 +1,17 @@
-../../01-qemu/src/main.rs
+#![feature(lang_items)]
+#![no_main]
+#![no_std]
+
+#[export_name = "_reset"]
+pub extern "C" fn main() -> ! {
+    let y;
+    let x = 42;
+    y = x;
+
+    loop {}
+}
+
+mod lang_items {
+    #[lang = "panic_fmt"]
+    extern "C" fn panic_fmt() {}
+}
